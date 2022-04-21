63e260a4dd7dfa31fa616709de8edc7f80008fe4
mohammadaminsaheb <mohammadaminsaheb@gmail.com>
2022-04-21 06:59:48 -0700

    install dependecy package and create folder stracture

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// Components
+import Card from '../src/components/Cards/Card'
+import Chart from '../src/components/Charts/Chart'
+import CountryPicker from '../src/components/CountryPicker/CountryPicker'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <>
+            <h1>app</h1>
+            <Card/>
+            <Chart/>
+            <CountryPicker/>
+            
+        </>
+    );
 }
-
+ 
 export default App;
